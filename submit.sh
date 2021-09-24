e3ddafc14d8100aad95f5171323eb1866da8b483
slenderham <wc199951@gmail.com>
2021-09-23 23:44:18 -0400

    bug fix

--- submit.sh
+++ submit.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 # Name of the job
-#SBATCH --job-name=gpu_job
+SBATCH --job-name=gpu_job
 
 # Number of compute nodes
-#SBATCH --nodes=1
+SBATCH --nodes=1
 
 # Number of cores, in this case one
-#SBATCH --ntasks-per-node=1
+SBATCH --ntasks-per-node=1
 
 # Request the GPU partition
-#SBATCH --partition gpuq
+SBATCH --partition gpuq
 
 # Request the GPU resources
-#SBATCH --gres=gpu:1
+SBATCH --gres=gpu:1
 
 # Walltime (job duration)
-#SBATCH --time=02:00:00
-
+SBATCH --time=02:00:00
 
 nvidia-smi
 echo $CUDA_VISIBLE_DEVICES
