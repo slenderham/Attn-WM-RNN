9711992e34243c4635848c0416f634ed076b7f22
slenderham <wc199951@gmail.com>
2021-09-26 13:53:15 -0400

    training still not working

--- cluster_submission/submit.sh
+++ cluster_submission/submit.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
 #SBATCH --time=08:00:00
-#SBATCH --mem=8GB
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mem=16GB
+#SBATCH --mail-type=END,FAIL
 
 cd /dartfs-hpc/rc/home/d/f005d7d/attn-rnn/Attn-WM-RNN
 ./run.sh
--- run.sh
+++ run.sh
@@ -2,13 +2,12 @@
 
 python rnn/train.py --cuda\
         --save_checkpoint\
-        --iters 10000\
+        --iters 1000\
         --batch_size 1\
         --stim_dim 3\
         --stim_val 3\
         --N_s 6\
         --init_spectral 1\
-	--learning_rate 1e-4\
         --add_attn\
         --activ_func retanh\
         --exp_dir exp/test
