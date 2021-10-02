82414ca27859d0ad6ddafa1c78289eb5807264c0
slenderham <wc199951@gmail.com>
2021-10-01 23:10:09 -0400

    change to xavier-ish init

--- rnn/models.py
+++ rnn/models.py
@@ -56,10 +56,10 @@ class EILinear(nn.Module):
 
     def reset_parameters(self, init_spectral, init_gain):
         with torch.no_grad():
-            nn.init.uniform_(self.weight, a=0, b=math.sqrt(1/(self.input_size-self.zero_cols)))
+            nn.init.uniform_(self.weight, a=0, b=math.sqrt(2/(self.output_size+self.input_size-self.zero_cols)))
             # Scale E weight by E-I ratio
-            # if self.i_size!=0:
-                # self.weight.data[:, :self.e_size] /= (self.e_size/self.i_size)
+            if self.i_size!=0:
+                self.weight.data[:, :self.e_size] /= (self.e_size/self.i_size)
 
             if init_gain is not None:
                 self.weight.data *= init_gain
