997b8ed9a401214117f6bc01f0fd9f81d843e98d
slenderham <wc199951@gmail.com>
2021-10-02 16:32:42 -0400

    change init

--- rnn/models.py
+++ rnn/models.py
@@ -56,7 +56,7 @@ class EILinear(nn.Module):
 
     def reset_parameters(self, init_spectral, init_gain):
         with torch.no_grad():
-            nn.init.uniform_(self.weight, a=0, b=math.sqrt(1/(self.output_size+self.input_size-self.zero_cols)))
+            nn.init.uniform_(self.weight, a=0, b=math.sqrt(2/(self.output_size+self.input_size-self.zero_cols)))
             # Scale E weight by E-I ratio
             if self.i_size!=0:
                 self.weight.data[:, :self.e_size] /= (self.e_size/self.i_size)
