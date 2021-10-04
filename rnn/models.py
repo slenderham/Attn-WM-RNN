b71579e92a24617c5b5398623b1e7d95b0a59b02
slenderham <wc199951@gmail.com>
2021-10-04 17:14:07 -0400

    bug fix

--- rnn/models.py
+++ rnn/models.py
@@ -166,7 +166,7 @@ class LeakyRNN(nn.Module):
         
         if self.attention:
             attn_weights = self.attn_func(output)
-            attn_weights = F.softmax(attn_weights, -1);
+            attn_weights = F.softmax(attn_weights, -1)
             attn_weights = torch.repeat_interleave(attn_weights, self.attn_group_size, dim=-1)
             x = torch.relu(x + self._sigma_in * torch.randn_like(x)) * attn_weights 
         else:
