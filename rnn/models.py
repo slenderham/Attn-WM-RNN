7d46349934103deec1aa43e7200bd0a9c6e51afc
slenderham <wc199951@gmail.com>
2021-10-07 11:30:18 -0400

    bug fix

--- rnn/models.py
+++ rnn/models.py
@@ -231,7 +231,7 @@ class HierarchicalRNN(nn.Module):
         self.hidden_size = hidden_size
         self.output_size =  output_size
         self.x2c = EILinear(input_size, hidden_size, remove_diag=False, pos_function='relu',
-                            e_prop=1, zero_cols_prop=0, bias=True, init_gain=0.5)
+                            e_prop=1, zero_cols_prop=0, bias=False, init_gain=0.5)
         self.c2c = EILinear(hidden_size, hidden_size, remove_diag=True, pos_function='relu',
                             e_prop=e_prop, zero_cols_prop=0, bias=True, init_gain=1, 
                             init_spectral=init_spectral, balance_ei=balance_ei)
