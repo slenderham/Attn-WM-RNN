ba83710e2bd9e35658e79de353bb9f1016d2ccf7
slenderham <wc199951@gmail.com>
2021-10-14 17:05:32 -0400

    change init

--- rnn/models.py
+++ rnn/models.py
@@ -99,7 +99,7 @@ class SimpleRNN(nn.Module):
         self.hidden_size = hidden_size
         self.output_size =  output_size
         self.x2h = EILinear(input_size, hidden_size, remove_diag=False, pos_function='relu',
-                            e_prop=1, zero_cols_prop=0, bias=False, init_gain=0.5)
+                            e_prop=1, zero_cols_prop=0, bias=False, init_gain=0.3)
         self.h2h = EILinear(hidden_size, hidden_size, remove_diag=True, pos_function='relu',
                             e_prop=e_prop, zero_cols_prop=0, bias=True, init_gain=1, 
                             init_spectral=init_spectral, balance_ei=balance_ei)
