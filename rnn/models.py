b17128e888137d71d2f58a1f5b558439523af13b
slenderham <wc199951@gmail.com>
2021-10-15 21:11:10 -0400

    make all lr positive

--- rnn/models.py
+++ rnn/models.py
@@ -238,13 +238,13 @@ class SimpleRNN(nn.Module):
             R = R.unsqueeze(-1)
             wx = wx * self.oneminusalpha_w + self.dt*R*(
                 self.multiply_blocks(torch.einsum('bi, bj->bij', new_output, x), \
-                    self.kappa_w[0:2*len(self.input_unit_group)], self.in_coords))
+                    self.kappa_w[0:2*len(self.input_unit_group)].abs(), self.in_coords))
             if self._sigma_w>0:
                 wx += self._sigma_w * torch.randn_like(wx)
             wx = torch.maximum(wx, -self.x2h.pos_func(self.x2h.weight).detach().unsqueeze(0))
             wh = wh * self.oneminusalpha_w + self.dt*R*(
                 self.multiply_blocks(torch.einsum('bi, bj->bij', new_output, output), \
-                    self.kappa_w[2*len(self.input_unit_group):2*len(self.input_unit_group)+4], self.rec_coords))
+                    self.kappa_w[2*len(self.input_unit_group):2*len(self.input_unit_group)+4].abs(), self.rec_coords))
             if self._sigma_w>0:
                 wh += self._sigma_w * torch.randn_like(wh)
             wh = torch.maximum(wh, -self.h2h.pos_func(self.h2h.weight).detach().unsqueeze(0))
