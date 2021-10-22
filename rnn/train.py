294c7f33f0ed8f9385efad42501496b38bfdb3d3
slenderham <wc199951@gmail.com>
2021-10-22 16:43:41 -0400

    bug fix

--- rnn/train.py
+++ rnn/train.py
@@ -218,7 +218,7 @@ if __name__ == "__main__":
                 losses.append(loss)
             losses_means = torch.cat(losses, dim=1).mean(1) # loss per trial
             losses_stds = torch.cat(losses, dim=1).std(1) # loss per trial
-            print('====> Epoch {} Eval Loss: {:.4f}'.format(epoch, losses_means[-args.stim_val**args.stim_dim:]))
+            print('====> Epoch {} Eval Loss: {:.4f}'.format(epoch, losses_means[-args.stim_val**args.stim_dim:].mean()))
             model.print_kappa()
             return losses_means, losses_stds
 
