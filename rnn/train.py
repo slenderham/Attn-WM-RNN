70002213540775ff7e0493257d3b7bc83902492b
slenderham <wc199951@gmail.com>
2021-10-24 17:47:28 -0400

    add back normalization

--- rnn/train.py
+++ rnn/train.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
                 rwd_go = (torch.rand_like(prob_s)<prob_s).reshape(args.stim_val**args.stim_dim*args.N_s, 1, args.batch_size).int()
                 rwd = output_mask['fixation']*((action==2).float()) + output_mask['target']*((rwd_go==action).float()-1*(action==2).float())
                 advantage = (rwd-value).detach()
-                # advantage = (advantage-advantage.mean())/(advantage.std()+1e-8)
+                advantage = (advantage-advantage.mean())/(advantage.std()+1e-8)
                 loss = - (m.log_prob(action)*advantage).mean() + args.beta_v*(rwd-value).pow(2).mean() - args.beta_entropy*m.entropy().mean()
             
             loss += args.l2r*hs.pow(2).mean() + args.l1r*hs.abs().mean()
