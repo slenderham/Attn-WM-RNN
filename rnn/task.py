d333520a267b69d76ab47d5e4672194eb3c4ada4
slenderham <wc199951@gmail.com>
2021-10-25 01:19:15 -0400

    bug fix

--- rnn/task.py
+++ rnn/task.py
@@ -146,7 +146,7 @@ class MDPRL():
         DA_s = DA_s.reshape((len(self.T)*len(index_s), batch_size, 1))
         # ch_s = ch_s.reshape((len(self.T)*len(index_s), batch_size, 1))
         pop_s = np.concatenate([pop_s, np.tile((self.T<0.0*self.s).reshape(1, len(self.T), 1, 1), (len(index_s), 1, batch_size, 1))], axis=-1)
-        pop_s = pop_s.reshape((len(self.T)*len(index_s), batch_size, 63))
+        pop_s = pop_s.reshape((len(self.T)*len(index_s), batch_size, 64))
         pop_o = pop_o.reshape((len(self.T)*len(index_s), batch_size, 27))
 
         if self.task=='value':
