2366f15ba38d7ddae2db969b49ce779e9c167a01
slenderham <wc199951@gmail.com>
2021-10-27 02:17:46 -0400

    switch to activity dependent scaling

--- rnn/models.py
+++ rnn/models.py
@@ -258,33 +258,29 @@ class SimpleRNN(nn.Module):
         if self.plastic:
             R = R.unsqueeze(-1)
             if self.in_coords is not None and self.rec_coords is not None:
-                wx = wx*self.oneminusalpha_w + self.x2h.pos_func(self.x2h.weight).unsqueeze(0)*self.alpha_w \
+                wx += -(wx-self.x2h.pos_func(self.x2h.weight).unsqueeze(0))*new_output.unsqueeze(1)*self.alpha_w \
                     + self.dt*R*(self.multiply_blocks(torch.einsum('bi, bj->bij', new_output, x), \
                         self.kappa_w[0:2*len(self.input_unit_group)].abs(), self.in_coords))
                 if self._sigma_w>0:
                     wx += self._sigma_w * torch.randn_like(wx)
-                # wx = torch.clamp(wx, 0, self.weight_bound)
-                wx = F.normalize(wx, p=1, dim=-1)
+                wx = torch.clamp(wx, 0, self.weight_bound)
                 # wx = torch.maximum(wx, -self.x2h.pos_func(self.x2h.weight).detach().unsqueeze(0))
                 # wx = torch.minimum(wx, self.weight_bound-self.x2h.pos_func(self.x2h.weight).detach().unsqueeze(0))
-                wh = wh*self.oneminusalpha_w + self.h2h.pos_func(self.h2h.weight).unsqueeze(0)*self.alpha_w \
+                wh += - (wh-self.h2h.pos_func(self.h2h.weight).unsqueeze(0))*new_output.unsqueeze(1)*self.alpha_w \
                     + self.dt*R*(self.multiply_blocks(torch.einsum('bi, bj->bij', new_output, output), \
                         self.kappa_w[2*len(self.input_unit_group):2*len(self.input_unit_group)+4].abs(), self.rec_coords))
                 if self._sigma_w>0:
                     wh += self._sigma_w * torch.randn_like(wh)
-                # wh = torch.clamp(wh, 0, self.weight_bound)
-                wh = torch.cat([torch.cat([F.normalize(wh[:, :self.h2h.e_size, :self.h2h.e_size], p=1, dim=-1),
-                               wh[:, :self.h2h.e_size, self.h2h.e_size:]], dim=-1), wh[:, self.h2h.e_size:, :]], dim=-2)
+                wh = torch.clamp(wh, 0, self.weight_bound)
                 # wh = torch.maximum(wh, -self.h2h.pos_func(self.h2h.weight).detach().unsqueeze(0))
                 # wh = torch.minimum(wh, self.weight_bound-self.h2h.pos_func(self.h2h.weight).detach().unsqueeze(0))
                 if self.plastic_feedback:
-                    wattn = wattn*self.oneminusalpha_w + self.attn_func.pos_func(self.attn_func.weight).unsqueeze(0)*self.alpha_w \
+                    wattn += - (wattn-self.attn_func.pos_func(self.attn_func.weight).unsqueeze(0))*attn.unsqueeze(1)*self.alpha_w \
                         + self.dt*R*(self.multiply_blocks(torch.einsum('bi, bj->bij', attn, output), \
                             self.kappa_w[2*len(self.input_unit_group)+4:3*len(self.input_unit_group)+4].abs(), self.fb_coords))
                     if self._sigma_w>0:
                         wattn += self._sigma_w * torch.randn_like(wattn)
-                    wattn = torch.cat([F.normalize(wattn[:,:,:self.attn_func.e_size], p=1, dim=-1), wattn[:,:,self.attn_func.e_size:]], dim=-1)
-                    # wattn = torch.clamp(wattn, 0, self.weight_bound)
+                    wattn = torch.clamp(wattn, 0, self.weight_bound)
                     # wattn = torch.maximum(wattn, -self.attn_func.pos_func(self.attn_func.weight).detach().unsqueeze(0))
                     # wattn = torch.minimum(wattn, self.weight_bound-self.h2h.pos_func(self.attn_func.weight).detach().unsqueeze(0))
             else:
