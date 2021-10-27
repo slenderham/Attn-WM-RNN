e5e4c3747a68dfa879c610d06d4399f93e0fc031
slenderham <wc199951@gmail.com>
2021-10-27 15:56:04 -0400

    try supervised learning

--- rnn/train.py
+++ rnn/train.py
@@ -171,21 +171,24 @@ if __name__ == "__main__":
             if args.task_type=='value':
                 loss = ((output.reshape(args.stim_val**args.stim_dim*args.N_s, output_mask.shape[1], args.batch_size, 1)-ch_s)*output_mask.unsqueeze(-1)).pow(2).mean()
             elif args.task_type=='off_policy':
-                log_p, value = output
+                log_p, _ = output
                 conj_mask = output_mask['target'].squeeze()==1
                 log_p = log_p.reshape(args.stim_val**args.stim_dim*args.N_s, output_mask['target'].shape[1], args.batch_size, 2)
                 log_p = log_p[:, conj_mask]
-                value = value.reshape(args.stim_val**args.stim_dim*args.N_s, output_mask['target'].shape[1], args.batch_size)
-                value = value[:, conj_mask]
-                m = torch.distributions.categorical.Categorical(logits=log_p)
-                action = m.sample().reshape(args.stim_val**args.stim_dim*args.N_s, conj_mask.sum().int(), args.batch_size)
-                rwd_go = (torch.rand_like(prob_s)<prob_s).reshape(args.stim_val**args.stim_dim*args.N_s, 1, args.batch_size).int()
-                rwd = ((rwd_go==action).float())
-                advantage = (rwd-value).detach()
-                value_loss = (rwd-value).pow(2)
-                entropy_loss = m.entropy()
+                # value = value.reshape(args.stim_val**args.stim_dim*args.N_s, output_mask['target'].shape[1], args.batch_size)
+                # value = value[:, conj_mask]
+                # m = torch.distributions.categorical.Categorical(logits=log_p)
+                # action = m.sample().reshape(args.stim_val**args.stim_dim*args.N_s, conj_mask.sum().int(), args.batch_size)
+                # rwd_go = (torch.rand_like(prob_s)<prob_s).reshape(args.stim_val**args.stim_dim*args.N_s, 1, args.batch_size).int()
+                # rwd = ((rwd_go==action).float())
+                # advantage = (rwd-value).detach()
+                # value_loss = (rwd-value).pow(2)
+                # entropy_loss = m.entropy()
                 # advantage = (advantage-advantage.mean())/(advantage.std()+1e-8)
-                loss = - (m.log_prob(action)*advantage).mean() + args.beta_v*value_loss.mean() - args.beta_entropy*entropy_loss.mean()
+                # loss = - (m.log_prob(action)*advantage).mean() + args.beta_v*value_loss.mean() - args.beta_entropy*entropy_loss.mean()
+                targets = (prob_s>0.5).reshape(args.stim_val**args.stim_dim*args.N_s, 1, args.batch_size).long()
+                targets = targets.repeat(1, output_mask['target'].shape[1], 1)
+                loss = F.cross_entropy(log_p.flatten(0, 2), targets.flatten())
             
             loss += args.l2r*hs.pow(2).mean() + args.l1r*hs.abs().mean()
             (loss/args.grad_accumulation_steps).backward()
