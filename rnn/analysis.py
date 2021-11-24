0f9057cb9618821b8e993d3b6b4f0a6dd5aaac0d
slenderham <wc199951@gmail.com>
2021-11-24 11:35:18 -0500

    added analysis code

---
+++ rnn/analysis.py
@@ -0,0 +1,70 @@
+from joblib.parallel import delayed
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score
+from scipy.optimize import curve_fit
+import pingouin as pg
+from joblib import Parallel, delayed
+from scipy.spatial.distance import pdist
+import math
+from tensorly.decomposition import parafac
+
+def run_pca(hs):
+    trials, timesteps, batch_size, hidden_dim = hs.shape
+    pca = PCA(n_components=3)
+    low_x = pca.fit_transform(hs.reshape(trials*timesteps*batch_size, hidden_dim)).reshape(trials, timesteps, batch_size, 3)
+    return low_x.reshape(trials, timesteps, batch_size, 3)
+
+def run_tca(ws):
+    trials, timesteps, batch_size, hidden_dim, hidden_dim = ws.shape
+    factors = parafac(ws.reshape(trials*timesteps*batch_size, hidden_dim, hidden_dim), rank=3)
+    low_w = factors[0]
+    return low_w.reshape(trials, timesteps, batch_size, 3)
+
+def linear_regression(X, y):
+    res = pg.linear_regression(X, y)
+    return res
+
+def linear_classification_cv(X, y):
+    clf = LinearSVC()
+    scores = cross_val_score(clf, X, y, cv=5)
+    return np.mean(scores)
+
+def forward_encoding(xs, hs, NPROC=16):
+    n_time_steps, n_trials, n_hidden = hs.shape
+    assert(xs.shape[:2]==(n_time_steps, n_trials))
+    results = [[None] * n_hidden] * n_time_steps
+    for t in range(n_time_steps):
+        jobs = [delayed(linear_regression)(xs[t], hs[t,:,i]) for i in range(n_hidden)]
+        results[t] = Parallel(n_jobs=NPROC)(jobs)
+    return results
+
+def backward_decoding(hs, ys, NPROC=16):
+    n_time_steps, n_trials, n_hidden = hs.shape
+    assert(ys.shape[:2]==(n_time_steps, n_trials))
+    jobs = [delayed(linear_classification_cv)(hs[t], ys[t]) for t in range(n_time_steps)]
+    results = Parallel(n_jobs=NPROC)(jobs)
+    return results
+    
+def representational_similarity_analysis(xs, hs, NPROC=16):
+    n_time_steps, n_trials, n_hidden = hs.shape
+    hs = hs.reshape((n_time_steps*n_trials, n_hidden))
+    rnn_sim = pdist(hs, metric='correlation')
+    input_sim = []
+    for x in xs: # for each input measure
+        x = x.reshape((n_time_steps*n_trials, 1))
+        input_sim.append(pdist(x, metric='euclidean'))
+    input_sim = np.stack(input_sim).transpose(1,0)
+    result = linear_regression(input_sim, rnn_sim)
+    return rnn_sim, input_sim, result
+
+def fit_exp(y,x=None):
+    if x is None:
+        x = np.arange(y.shape[0])
+    pred = lambda p: 0+(p[0]-0)*(1-math.exp(-x/p[1]))
+    p0 = [np.rand()*10, np.rand()*1e4]
+    lb = [0, 0]
+    ub = [10, 1e4]
+    popt, _ = curve_fit(pred, x, y, bound=(lb, ub), p0=p0)
+    return popt
--- rnn/plot_main.py
+++ rnn/plot_main.py
@@ -2,18 +2,24 @@ from collections import defaultdict
 from matplotlib import pyplot as plt
 from torch.nn.functional import interpolate
 from torch.serialization import save
+from analysis import representational_similarity_analysis
 from utils import load_checkpoint
 from models import SimpleRNN
 from task import MDPRL
+from analysis import *
 import torch
 import os
 import json
 import math
 import numpy as np
 
-def plot_mean_and_std(ax, m, sd):
-    ax.plot(m)
+def plot_mean_and_std(ax, m, sd, label):
+    ax.plot(m, label=label)
     ax.fill_between(range(len(m)), m-sd, m+sd, alpha=0.1)
+
+def plot_imag_centered_cm(ax, im):
+    max_mag = im.abs().max()
+    ax.imshow(im, vmax=max_mag, vmin=-max_mag, colormap='RdBu')
 
 def plot_connectivity(x2hw, h2hw, hb, h2ow):
     maxmax = abs(max([x2hw.max().item(), h2hw.max().item(), hb.max().item(), h2ow.max().item()]))
@@ -41,10 +47,11 @@ def plot_connectivity(x2hw, h2hw, hb, h2ow):
 
 def plot_learning_curve(lm, lsd):
     fig, ax = plt.subplots()
-    plot_mean_and_std(ax, lm, lsd)
-    ax.vlines(x=args['N_s']*args['stim_val']**args['stim_dim'], ymin=0, ymax=(lm+lsd).max()*1.05)
+    plot_mean_and_std(ax, lm[0], lsd[0], label='Percent Better')
+    plot_mean_and_std(ax, lm[1], lsd[1], label='Reward')
+    ax.vlines(x=args['N_s']*args['stim_val']**args['stim_dim'], ymin=0, ymax=(lm+lsd).max()*1.05, colors='black')
     ax.set_xlabel('Trials')
-    ax.set_ylabel('Loss')
+    ax.set_ylabel('Percent Correct')
     plt.tight_layout()
     plt.savefig('plots/learning_curve')
 
@@ -73,29 +80,101 @@ def plot_attn_distribution(attns):
     plt.show()
     # plt.savefig('plots/attn_bars')
 
+def plot_sorted_matrix(w, selectivity, e_size):
+    # from https://github.com/gyyang/nn-brain/blob/master/EI_RNN.ipynb
+    ind_sort = np.concatenate((np.argsort(selectivity[:e_size]), np.argsort(selectivity[e_size:])+e_size))
+    w = w[ind_sort, :][:, ind_sort]
+    ax = plt.subplot(111)
+    plot_imag_centered_cm(ax, w)
+    return w
+
+def plot_rsa(hs, inputs, stim_order, stim_probs, splits=10):
+    n_timesteps, n_batch, n_hidden = hs.shape
+    n_steps_par_split = n_timesteps//splits
+    probs_by_feat, probs_by_conj, probs_by_obj = stim_probs
+    probs_by_feat = probs_by_feat[stim_order[0]] - probs_by_feat[stim_order[1]]
+    probs_by_conj = probs_by_conj[stim_order[0]] - probs_by_conj[stim_order[1]]
+    probs_by_obj = probs_by_obj[stim_order[0]] - probs_by_obj[stim_order[1]]
+    rnn_sims = []
+    input_sims = []
+    reg_results = defaultdict(list)
+    for i in range(splits-1):
+        xs = np.concatenate([probs_by_feat[i*n_steps_par_split:(i+1)*n_steps_par_split],
+              probs_by_conj[i*n_steps_par_split:(i+1)*n_steps_par_split],
+              probs_by_obj[i*n_steps_par_split:(i+1)*n_steps_par_split]], axis=-1).transpose(1,0) # 
+        rnn_sim, input_sim, reg_result = representational_similarity_analysis(xs, hs[i*n_steps_par_split:(i+1)*n_steps_par_split])
+        rnn_sims.append(rnn_sim)
+        input_sims.append(input_sim)
+        reg_results.append(reg_result)
+    
+    plt.subplot(111).plot()
+
 def run_model(args, model, task_mdprl):
     model.eval()
-    losses = []
-    all_saved_states = defaultdict(list)
+    accs = []
+    rwds = []
+    all_indices = []
+    all_saved_states_pre = defaultdict(list)
+    all_saved_states_post = defaultdict(list)
     with torch.no_grad():
         for i in range(10):
             print(i)
-            DA_s, ch_s, pop_s, index_s, output_mask = task_mdprl.generateinputfromexp(args['batch_size'], 10)
-            total_input = pop_s
-            output, hs, saved_states = model(total_input, DA_s, save_attn=True)
-            for k, v in saved_states.items():
-                all_saved_states[k].append(v)
-            all_saved_states['hs'].append(hs)
-            
-            output = output.reshape(args['stim_val']**args['stim_dim']*10, output_mask.shape[1], args['batch_size']) # trial X T X batch size
-            loss = (output[:, output_mask.squeeze()==1]-ch_s[:, output_mask.squeeze()==1].squeeze(-1)).pow(2).mean(1) # trial X batch size
-            losses.append(loss)
+            DA_s, ch_s, pop_s, index_s, prob_s, output_mask = task_mdprl.generateinputfromexp(1, args['test_N_s'])
+            if args['task_type']=='value':
+                output, hs, _ = model(pop_s, DA_s)
+                output = output.reshape(args['stim_val']**args['stim_dim']*args['test_N_s'], output_mask.shape[1], 1) # trial X T X batch size
+                loss = (output[:, output_mask.squeeze()==1]-ch_s[:, output_mask.squeeze()==1].squeeze(-1)).pow(2).mean(1) # trial X batch size
+            else:
+                acc = []
+                curr_rwd = []
+                hidden = None
+                for i in range(len(pop_s['pre_choice'])):
+                    # first phase, give stimuli and no feedback
+                    output, hs, hidden, ss = model(pop_s['pre_choice'][i], hidden=hidden, 
+                                                Rs=0*DA_s['pre_choice'], Vs=None,
+                                                acts=torch.zeros(args['batch_size'], 2)*DA_s['pre_choice'], 
+                                                save_attns=True, save_weights=True)
+                    for k, v in ss.items():
+                        all_saved_states_pre[k].append(v)
+                    all_saved_states_pre['hs'].append(hs)
+
+                    # use output to calculate action, reward, and record loss function
+                    logprob, value = output
+                    m = torch.distributions.categorical.Categorical(logits=logprob[-1])
+                    action = m.sample().reshape(args['batch_size'])
+                    rwd = (torch.rand(args['batch_size'])<prob_s[i][range(args['batch_size']), action]).float()
+                    acc.append((torch.argmax(logprob[-1], -1)==torch.argmax(prob_s[i], -1)).float())
+                    curr_rwd.append(rwd)
+                    # use the action (optional) and reward as feedback
+                    pop_post = pop_s['post_choice'][i]
+                    action_enc = torch.eye(2)[action]
+                    pop_post = pop_post*action_enc.reshape(1,1,2,1)
+                    action_enc = action_enc*DA_s['post_choice']
+                    R = (2*rwd-1)*DA_s['post_choice']
+                    if args['rpe']:
+                        V = value[-1]*DA_s['post_choice']
+                    else:
+                        V = None
+                    _, hs, hidden, ss = model(pop_post, hidden=hidden, Rs=R, Vs=V, acts=action_enc, save_attns=True, save_weights=True)
+                    for k, v in ss.items():
+                        all_saved_states_post[k].append(v)
+                    all_saved_states_post['hs'].append(hs)
+                acc = torch.stack(acc, dim=0)
+                curr_rwd = torch.stack(curr_rwd, dim=0)
+            accs.append(acc)
+            rwds.append(curr_rwd)
+            all_indices.append(index_s)
         
-        losses_means = torch.cat(losses, dim=1).mean(1) # loss per trial
-        losses_stds = torch.cat(losses, dim=1).std(1) # loss per trial
-        for k, v in saved_states.items():
-            all_saved_states[k] = torch.cat(v, dim=1)
-        return losses_means, losses_stds, all_saved_states
+        accs_means = torch.cat(accs, dim=1).mean(1) # loss per trial
+        accs_stds = torch.cat(accs, dim=1).std(1) # loss per trial
+        rwds_means = torch.cat(rwds, dim=1).mean(1) # loss per trial
+        rwds_stds = torch.cat(rwds, dim=1).std(1) # loss per trial
+        all_saved_states = {}
+        for k in all_saved_states_pre.keys():
+            all_saved_states[k] = torch.cat([torch.cat(all_saved_states_pre[k], dim=1),
+                                             torch.cat(all_saved_states_post[k], dim=1)], dim=0)
+        all_indices = torch.stack(all_indices, dim=1) # trials X batch size X 2
+        return [accs_means, rwds_means], [accs_stds, rwds_stds], all_saved_states, all_indices
 
 
 # TODO: use FC to characterize feature and object selection neurons, as predicted by the hierarchical model
@@ -110,6 +189,7 @@ if __name__=='__main__':
     parser.add_argument('--connectivity', action='store_true')
     parser.add_argument('--tensor_decomp', action='store_true')
     parser.add_argument('--pca', action='store_true')
+    parser.add_argument('--rsa', action='store_true')
     parser.add_argument('--learning_curve', action='store_true')
     parser.add_argument('--attn_entropy', action='store_true')
     parser.add_argument('--attn_distribution', action='store_true')
@@ -170,7 +250,7 @@ if __name__=='__main__':
     model.load_state_dict(state_dict)
     print('loaded model')
 
-    losses_means, losses_stds, all_saved_states = run_model(args, model, task_mdprl)
+    losses_means, losses_stds, all_saved_states, all_indices = run_model(args, model, task_mdprl)
     print('simulation complete')
     
     # load metrics
@@ -187,3 +267,5 @@ if __name__=='__main__':
         plot_attn_entropy(all_saved_states['attns'])
     if plot_args.attn_distribution:
         plot_attn_distribution(all_saved_states['attns'])
+    if plot_args.rsa:
+        plot_rsa(all_saved_states['hs'], task_mdprl.value_est(), stim_order=all_indices, stim_probs=task_mdprl.prob_mdprl)
